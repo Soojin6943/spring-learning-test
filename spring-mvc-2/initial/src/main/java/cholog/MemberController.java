e000229bec6d1636b2613b6c02c41df09bfa3d16
Soojin6943 <sjhan0711@gmail.com>
2026-04-27 22:50:55 +0900

    fix(spring-mvc-2): delete

--- spring-mvc-2/initial/src/main/java/cholog/MemberController.java
+++ spring-mvc-2/initial/src/main/java/cholog/MemberController.java
@@ -44,15 +44,14 @@ public class MemberController {
     }
 
     @DeleteMapping("/members/{id}")
-    public ResponseEntity<Void> delete() {
-        // TODO: url 상의 id 정보를 받아 member를 삭제한다.
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         Member member = members.stream()
-            .filter(it -> Objects.equals(it.getId(), null))
+            .filter(it -> Objects.equals(it.getId(), id))
             .findFirst()
             .orElseThrow(RuntimeException::new);
 
         members.remove(member);
 
-        return null;
+        return ResponseEntity.noContent().build();
     }
 }
